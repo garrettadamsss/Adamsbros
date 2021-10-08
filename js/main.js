b3af64863806083607cdaf17f21d541f8e348d34
garrettadamsss <garrettadamssss@gmail.com>
2021-10-07 17:07:33 -0700

    adding adams_logo, js file, and slide image folder

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//default setting
+let slideIndex = 1;
+showSlide(slideIndex);
+
+//next and previous controls
+function moveSlides(n){
+    slideIndex += n
+    showSlide(slideIndex);
+}
+// dots controls 
+function currentSlide(n){
+    slideIndex = n
+    showSlide(slideIndex); 
+}
+function showSlides(index){
+    var slides = document.getElementsByClassName("slides");
+    
+    if(index < slides.length){
+
+    }
+}
